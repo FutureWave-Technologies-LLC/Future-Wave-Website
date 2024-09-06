a64c2f9744ee0ae3fb30f097003c74cd76499f89
ayecmon <112906516+ayecmon@users.noreply.github.com>
2024-09-05 21:15:46 -0700

    added aboutUs page and log page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import { useNavigate } from 'react-router-dom';
 
-function AboutPage() {
+function AboutUsPage() {
+    const navigate = useNavigate();
+    
+    function goToHomePage() {
+        navigate("/");
+    }
+
     return (
-        <>
-            <p>Hi, I'm the about page! I need information.</p>
-        </>
-    );
+        <div className="AboutUs">
+            <h1>About FutureWave LLC:</h1>
+            <p>At FutureWave LLC, we are more than just developers—we are digital architects, crafting innovative web solutions that empower your business to thrive in the online world. With a team of passionate, forward-thinking software engineers, we specialize in creating bespoke websites and web applications that blend cutting-edge technology with seamless user experiences.
+
+Our engineers excel in both frontend and backend development, ensuring every aspect of your digital presence is meticulously crafted and optimized for performance. Whether it's stunning interfaces or complex systems powered by robust databases, FutureWave delivers solutions that are not only functional but transformative.
+
+We believe in pushing boundaries, working hard, and leveraging our years of experience to bring your vision to life. With FutureWave, your business is always riding the crest of innovation. Join us, and let's build a future where your digital ambitions know no limits..</p>
+            <button onClick={goToHomePage}>Back to Home</button>
+        </div>
+    )
 }
 
-export default AboutPage;
+export default AboutUsPage;
--- src/pages/LogsPage.tsx
+++ src/pages/LogsPage.tsx
@@ -1,9 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+
 function LogsPage() {
+    const navigate = useNavigate();
+    
+    function goToHomePage() {
+        navigate("/");
+    }
+
     return (
-        <>
-            <p>Hi, I'm the logs page</p>
-        </>
-    );
+        <div className="Logs">
+            <h1>Logs</h1>
+            <a href="https://docs.google.com/document/d/1YmoW6RroP7YpzDHo0FVBTEss3hRd-xOF86gJBfo1W4A/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View Logs</a>
+            <br />
+            <button onClick={goToHomePage}>Back to Home</button>
+        </div>
+    )
 }
 
 export default LogsPage;
